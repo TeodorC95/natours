919046c3712d400ae86662df1a99d56b0f06f626
TeodorC95 <teobl95@gmail.com>
2021-08-31 18:46:30 +0200

    s9v111 handling unhandled routes

--- app.js
+++ app.js
@@ -16,11 +16,6 @@ app.use(express.json()); //middleware
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware 🎈🎈🎈");
-  next();
-});
-
-app.use((req, res, next) => {
   req.requrestTime = new Date().toISOString();
   next();
 });
@@ -29,6 +24,13 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 // 4) START SERVER
 module.exports = app;
