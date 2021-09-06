0533d668f8f62b4ea771e0627d5f088d91153261
TeodorC95 <teobl95@gmail.com>
2021-09-06 19:43:54 +0200

    v113 implementing a global error hangling middleware

--- app.js
+++ app.js
@@ -26,9 +26,24 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server!`,
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  // to define an error handling middleware, we need to add 4 arguments, and this function is error first function
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
