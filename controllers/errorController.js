3ad948912eda221f411c103e13732b69cc4fd6c4
TeodorC95 <teobl95@gmail.com>
2021-09-06 21:41:36 +0200

    dev vs prod errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,32 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error - send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error("ERROR 🧨", err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong!",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // to define an error handling middleware, we need to add 4 arguments, and this function is error first function
   // console.log(err.stack);
@@ -5,8 +34,10 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  }
+  if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  }
 };
