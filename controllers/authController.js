536844b0d45cc849dc82dc521048f5440e252c97
TeodorC95 <teobl95@gmail.com>
2021-09-17 17:51:57 +0200

    v131 protecting routes v2

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
 const catchAsync = require("../utils/catchAsync");
@@ -15,6 +16,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
   });
 
   const token = signToken(newUser._id);
@@ -65,10 +67,25 @@ exports.protect = catchAsync(async (req, res, next) => {
     return next(
       new AppError("You are not logged in! Please log in to get access", 401)
     );
+
   // 2) Validate token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  console.log(decoded);
 
   // 3) Check if user still exists
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser)
+    return next(
+      new AppError("The user belonging to this token does no longer exist."),
+      401
+    );
 
   // 4) Check if user changed password after the token was issued
+  if (freshUser.changedPasswordAfter(decoded.iat))
+    return next(
+      new AppError("User recently changed password. Please log in again!", 401)
+    );
+
+  req.user = freshUser;
   next();
 });
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -16,6 +16,12 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+  new AppError("Invalid token. Please log again!", 401);
+
+const handleJWTExpiredError = () =>
+  new AppError("Token has expired. Please log in again!", 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -61,6 +67,11 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === "ValidationError")
       error = handleValidationErrorDB(error);
+
+    if (error.name === "JsonWebTokenError") error = handleJWTError(error);
+    if (error.name === "TokenExpiredError")
+      error = handleJWTExpiredError(error);
+
     sendErrorProd(error, res);
   }
 };
