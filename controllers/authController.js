ef740ca974697f2d8746fe36be2814ced7a43f6a
TeodorC95 <teobl95@gmail.com>
2021-09-19 12:24:14 +0200

    v134 password reset: reset token

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("./../utils/appError");
+const { AsyncLocalStorage } = require("async_hooks");
 
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -102,3 +103,18 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user)
+    return next(new AppError("There is no user with that email.", 404));
+
+  // 2) Generate the random token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3) Send it to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
